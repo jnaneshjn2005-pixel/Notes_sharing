05719b890f70a9e5e5fee88177a168e83f75cbf0
jnaneshjn2005-pixel <jnaneshjn2005@gmail.com>
2026-01-21 19:57:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+function getNotes() {
+  return JSON.parse(localStorage.getItem("notes")) || [];
+}
+
+function saveNotes(notes) {
+  localStorage.setItem("notes", JSON.stringify(notes));
+}
+
 /* ===============================
    LOGIN
 ================================ */
